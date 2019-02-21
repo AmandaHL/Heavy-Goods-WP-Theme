89f74460ec869efff372f5c8605c872d21978ded
Amanda Long <amanda@designformation.co>
2019-02-21 14:43:26 -0700

    Update 404.php

--- 404.php
+++ 404.php
@@ -4,21 +4,13 @@
 		<!-- section -->
 		<section>
 
-			<!-- article -->
-			<article id="post-404">
-
-				<h1><?php _e( 'Page not found', 'html5blank' ); ?></h1>
-				<h2>
-					<a href="<?php echo home_url(); ?>"><?php _e( 'Return home?', 'html5blank' ); ?></a>
-				</h2>
-
-			</article>
-			<!-- /article -->
-
+				<div class="content not-found">
+						<h1><?php _e( 'Not Found', 'mcocp' ); ?></h1>
+						<p><?php _e( 'It looks like nothing was found at this location. Check the page URL and try again.', 'heavygoods' ); ?></p>
+						
+					</div><!--.content-->
 		</section>
 		<!-- /section -->
 	</main>
-
-<?php get_sidebar(); ?>
 
 <?php get_footer(); ?>
